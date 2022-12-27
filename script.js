e0a00a90b119374cf83096bf9a372478a3f25efe
Anders Greve Sørensen <a.grevesoerensen@gmail.com>
2022-12-27 20:43:13 +0100

    Add function to play 1 round of RPS

--- script.js
+++ script.js
@@ -1,10 +1,36 @@
 function getComputerChoice() {
     let num = Math.floor(Math.random() * 3)
-    if (num == 0) { return "Rock" }
-    else if (num == 1) { return "Paper" }
-    else { return "Scissors" }
+    if (num == 0) { return "rock" }
+    else if (num == 1) { return "paper" }
+    else { return "scissors" }
 }
 
-// let number = Math.floor(Math.random() * 3)
+function playRound(playerSelection, computerSelection) {
+    let playerChoice = playerSelection.toLowerCase()
+    if (playerChoice === computerSelection) {
+        return `Draw! You both selected ${playerChoice}`
+    }
+    else if (playerChoice == "paper" && computerSelection == "rock") {
+        return "You Win! Paper beats Rock."
+    }
+    else if (playerChoice == "paper" && computerSelection == "scissors") {
+        return "You Lose! Scissors beats Paper"
+    }
+    else if (playerChoice == "rock" && computerSelection == "scissors") {
+        return "You Win! Rock beats Scissors!"
+    }
+    else if (playerChoice == "rock" && computerSelection == "paper") {
+        return "You Lose! Paper beats Rock"
+    }
+    else if (playerChoice == "scissors" && computerSelection == "paper") {
+        return "You Win! Scissors beats Paper"
+    }
+    else if (playerChoice == "scissors" && computerSelection == "rock") {
+        return "You Lose! Rock beats Scissors"
+    }
+    // else return "You Lose! Rock beats Scissors"
+}
 
-console.log(getComputerChoice())
+console.log(playRound("sCiSsoRs", getComputerChoice()))
+
+function game()
